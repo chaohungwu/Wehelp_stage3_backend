5225c0fbe81134cc643a9a5c00d021419d7e0f2b
chaohungwu <asp5432101@gmail.com>
2025-05-11 02:16:25 +0800

    fix upload same name  will overwrite same image where in DB

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter, HTTPException
-from model.DB_ORM import DBfunction
+# from fastapi import APIRouter, HTTPException
+# from model.DB_ORM import DBfunction
 
 
-DBfunction = DBfunction()
-GetMessage = DBfunction.Messagesearch()
-print(GetMessage)
+# DBfunction = DBfunction()
+# GetMessage = DBfunction.Messagesearch()
+# print(GetMessage)
+
+
+from datetime import datetime
+currentDateAndTime = datetime.now()
+
+
