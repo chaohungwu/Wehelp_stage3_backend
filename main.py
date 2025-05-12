0bcc98a22b6992cb188ad9295c0e6705513c49ea
chaohungwu <asp5432101@gmail.com>
2025-05-12 23:23:39 +0800

    modify website url path

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 app.include_router(router)
 
-@app.get("/", include_in_schema=False)
+@app.get("/messageboard", include_in_schema=False)
 async def index(request: Request):
 	return FileResponse("./static/index.html", media_type="text/html")
 
-app.mount("/", StaticFiles(directory="static" ,html=True))#所有靜態文件資料夾
+app.mount("/static", StaticFiles(directory="static"))#所有靜態文件資料夾
